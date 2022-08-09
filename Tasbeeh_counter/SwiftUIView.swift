4d24272ae2aeb5848d132746c1a972da3268c684
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2022-08-10 02:12:28 +0300

    first commit

---
+++ Tasbeeh_counter/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Tasbeeh_counter
+//
+//  Created by SHUAA on 02/12/1443 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
--- Tasbeeh_counter/ViewController.swift
+++ Tasbeeh_counter/ViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var countTxt = 0
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func startCountButton(_ sender: Any) {
+        countTxt += 1
+           counterLabel.text = ("\(countTxt)")
+    }
+    @IBAction func restartButton(_ sender: Any) {
+        countTxt = 0
+        counterLabel.text = ("\(countTxt)")
+    }
     override func viewDidLoad() {
+        countTxt = 0
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
